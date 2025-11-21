95f19a250056af0537b32229bcd869d1ca57ed6c
Jorge Matos <jisapjisap@gmail.com>
2025-11-21 16:14:38 +0100

    feat: implement homepage with featured events using the new EventCard component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
 import React from 'react'
+
+const events = [
+  { image: '/images/event1.png', title: 'Event 1', slug: 'event-1', location: 'Location 1', date: 'Date 1', time: 'Time 1' },
+  { image: '/images/event2.png', title: 'Event 2', slug: 'event-2', location: 'Location 2', date: 'Date 2', time: 'Time 2' },
+  { image: '/images/event3.png', title: 'Event 3', slug: 'event-3', location: 'Location 3', date: 'Date 3', time: 'Time 3' },
+]
+
 
 const Home = () => {
   return (
@@ -13,9 +21,9 @@ const Home = () => {
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {[1, 2, 3, 4, 5].map((event) => (
-            <li key={event}>
-              Event: {event}
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
             </li>
           ))}
         </ul>

e0bfac6a81d6fd38fc9bb6116be1c610795f331f
Jorge Matos <jisapjisap@gmail.com>
2025-11-21 15:22:54 +0100

    feat: Initialize Next.js project with core pages, utilities, MongoDB setup, and animated background.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <section>
+      <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+    </section>
   )
 }
 
